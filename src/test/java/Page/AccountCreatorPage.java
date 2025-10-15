98fb177ddc97bac9d984a12607b92546851ae91e
Priya-Pinky <chepuripriyanka1110@gmail.com>
2025-10-15 12:09:50 +0530

    Update AccountCreatorPage.java

--- src/test/java/Page/AccountCreatorPage.java
+++ src/test/java/Page/AccountCreatorPage.java
@@ -4,5 +4,7 @@ public class AccountCreatorPage
   {
     @FindBy(xpath="http:www.com") WebElement userName;
      @FindBys(xpath="http:www.com")List<WebElement> password;
+         @FindBys(xpath="http:www.com")List<WebElement> data2;
+
 
 }
