880e0a44355f9c83afc690f362110bc208c03e56
Priya-Pinky <chepuripriyanka1110@gmail.com>
2025-10-15 12:58:07 +0530

    changes

--- src/test/java/Page/AccountCreatorPageDemo.java
+++ src/test/java/Page/AccountCreatorPageDemo.java
@@ -3,6 +3,8 @@ package Page;
 public class AccountCreatorPageDemo
   {
     @FindBy(xpath="http:www.com") WebElement userName;
+    @FindBy(xpath="http:www.com") WebElement uuserName;
+
      @FindBys(xpath="http:www.com")List<WebElement> password;
      @FindBys(xpath="http:www.com")List<WebElement> data;
          @FindBys(xpath="http:www.com")List<WebElement> data2;
--- src/test/java/Page/CustomerIdCreationPage.java
+++ src/test/java/Page/CustomerIdCreationPage.java
@@ -2,6 +2,7 @@ package Page;
 
 public class CustomerIdCreationPage 
 {
+@FindBy(xpath="http:www.com") WebElement modifying;
   @FindBy(xpath="http:www.com") WebElement enterData;
      @FindBys(xpath="http:www.com")List<WebElement> enterurl;
 
